21ddb19ecf95d8a6657f88d897a1fc18be7d57e5
Augusto Renan <augustorenanss@gmail.com>
2020-08-09 15:09:18 -0300

    feat: make EmailService conforms to a protocol

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -1,5 +1,16 @@
-class EmailService {
-  sendMail(to, message) {
+interface IMailTo {
+  name: string
+  email: string
+}
 
+interface IMailMessage {
+  subject: string
+  body: string
+  attachment?: string[]
+}
+
+class EmailService {
+  sendMail(to: IMailTo, message: IMailMessage) {
+    console.log(`Mail send to ${to.name}: ${message.body}`)
   }
 }
